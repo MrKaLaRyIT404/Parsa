b8e3649a86b31a384a8614cb1cd5bccdb5052b8b
MrKaLaRyIT404 <110169656+MrKaLaRyIT404@users.noreply.github.com>
2026-05-14 14:50:35 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-$conn = new mysqli("localhost","root","","parsa_concrete");
+$conn = new mysqli("51.83.49.125","u173416_rywFYQoI0t","B!Fsov25TGrhvpkw=CqrcnCP","s173416_db1778759243275");
 if($conn->connect_error){ die("DB Error"); }
 ?>
